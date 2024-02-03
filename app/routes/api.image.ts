367de9d0d1c483fdfd2dd19afb4663acafa2616d
Silas Krause <krausesilas@gmail.com>
2024-02-03 02:30:54 +0100

    Add cache header

--- app/routes/api.image.ts
+++ app/routes/api.image.ts
@@ -1,11 +1,19 @@
 import { type LoaderFunction } from "@remix-run/cloudflare";
-import { imageLoader, MemoryCache } from "remix-image/serverPure";
+import { imageLoader, LoaderConfig, MemoryCache } from "remix-image/serverPure";
 
-export const loader: LoaderFunction = ({ request }) => {
-	const clonedRequest = new Request(request)
-	const config = {
-		selfUrl: "http://"+clonedRequest.headers.get("host") ?? "localhost:8788",
+export const loader: LoaderFunction = async ({ request }) => {
+	const config: LoaderConfig = {
+		selfUrl: "http://"+request.headers.get("host") ?? "localhost:8788",
 		cache: new MemoryCache(),
 	}
-  return imageLoader(config, clonedRequest)
+	const response = await imageLoader(config, request)
+
+  return new Response(response.body, { 
+		headers: {
+			...response.headers,
+			"Cache-Control": "max-age=3600, s-max-age=86400, max-stale=31536000",
+		},
+		status: response.status,
+		statusText: response.statusText
+	})
 }
